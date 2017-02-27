524c0439328bdcab948623bb8c4cf5d07d5ff13d
yadro <yadrone@gmail.com>
2017-02-27 23:48:45 +0300

    add cross origin access

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 
 app.get('/', function (req, res) {
